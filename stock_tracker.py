915ff0aa5768618e881a18352ea1453435107ccf
akshara-coder20 <amodakshara@gmail.com>
2026-06-14 18:27:55 +0530

    Add stock portfolio tracker functionality

--- stock_tracker.py
+++ stock_tracker.py
@@ -1,1 +1,24 @@
+# Stock Portfolio Tracker
 
+stock_prices = {
+    "AAPL": 180,
+    "TSLA": 250
+}
+
+print("===== Stock Portfolio Tracker =====")
+
+stock_name = input("Enter stock name (AAPL/TSLA): ").upper()
+quantity = int(input("Enter quantity: "))
+
+if stock_name in stock_prices:
+
+    total_value = stock_prices[stock_name] * quantity
+
+    print("\n----- Portfolio Details -----")
+    print("Stock Name:", stock_name)
+    print("Price per Share: $", stock_prices[stock_name])
+    print("Quantity:", quantity)
+    print("Total Investment Value: $", total_value)
+
+else:
+    print("Stock not found!")
